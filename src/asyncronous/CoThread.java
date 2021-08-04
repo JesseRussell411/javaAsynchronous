6f44d0e548f7289a3eb4588dc6279c9fd2bac01b
jesse russell <jesserussell4@montana.edu>
2021-08-04 15:29:44 -0600

    fixed garbage collection issue

--- src/asyncronous/CoThread.java
+++ src/asyncronous/CoThread.java
@@ -3,28 +3,28 @@ package asyncronous;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 
+
 class CoThreadHolder<T> implements AutoCloseable{
 	private T result = null;
 	private Thread thread = null;
 	private AtomicBoolean complete = new AtomicBoolean(false);
 	private AtomicBoolean threadPaused = new AtomicBoolean(false);
 	private AtomicBoolean started = new AtomicBoolean(false);
-	
-	private static class YieldInterruptedException extends RuntimeException{
-		private static final long serialVersionUID = 1L;}
+	@SuppressWarnings("serial")
+	private static class YieldInterruptedException extends RuntimeException{}
 	
 	private synchronized void yield(T result) {
 		this.result = result;
 		threadPaused.set(true);
-        notify();
-        try{
-        	while(threadPaused.get()){
-        		wait();
-        	}
-        }
-        catch(InterruptedException e){
-        	throw new YieldInterruptedException();
-        }
+		notify();
+		try{
+			while(threadPaused.get()){
+				wait();
+			}
+		}
+		catch(InterruptedException e){
+			throw new YieldInterruptedException();
+		}
 	}
 	
 	
@@ -37,29 +37,29 @@ class CoThreadHolder<T> implements AutoCloseable{
 		if (routine == null) { throw new NullPointerException(); }
 		
 		thread = new Thread(() -> {
-        	synchronized(this) {
-        		// wait for start() call
-        		try{
-		            while(threadPaused.get()){
-	                    wait();
-		            }
-        		} catch(InterruptedException e){
-        			complete.set(true);
-        			notify();
-        			return;
-        		}
-	            
-        		// start routine
-	            try {
-	            	routine.accept(r -> this.yield(r));
-	            }
-	            catch(YieldInterruptedException e) {}
-	            
-	            // routine is complete (or interrupted)
-	            complete.set(true);
-	            notify();
-        	}
-        });
+			synchronized(this) {
+				// wait for start() call
+				try{
+					while(threadPaused.get()){
+						wait();
+					}
+				} catch(InterruptedException e){
+					complete.set(true);
+					notify();
+					return;
+				}
+				
+				// start routine
+				try {
+					routine.accept(r -> this.yield(r));
+				}
+				catch(YieldInterruptedException e) {}
+				
+				// routine is complete (or interrupted)
+				complete.set(true);
+				notify();
+			}
+		});
 	}
 	
 	
@@ -67,42 +67,42 @@ class CoThreadHolder<T> implements AutoCloseable{
 		if (started()) {
 			return;
 		}
-        
-        thread.start();
-        started.set(true);
-        
-        threadPaused.set(false);
-        notify();
-        while(!threadPaused.get() && !complete.get()){
-            wait();
-        }
+		
+		thread.start();
+		started.set(true);
+		
+		threadPaused.set(false);
+		notify();
+		while(!threadPaused.get() && !complete.get()){
+			wait();
+		}
 	}
 	
 	public static class CoThreadNotStartedException extends RuntimeException{
 		private static final long serialVersionUID = 1L;
-
+		
 		public CoThreadNotStartedException(){
-            super("CoThread was not started yet.");
-        }
-    }
+			super("CoThread was not started yet.");
+		}
+	}
 	
 	public synchronized T await() throws InterruptedException{
 		if (!started()) { throw new CoThreadNotStartedException(); }
 		if (complete.get()) { return null; }
 		
-        
-        T result = this.result;
-        
-        
-        threadPaused.set(false);
-        notify();
-        while(!threadPaused.get() && !complete.get()){
-            wait();
-        }
-        
-        return result;
+		
+		T result = this.result;
+		
+		
+		threadPaused.set(false);
+		notify();
+		while(!threadPaused.get() && !complete.get()){
+			wait();
+		}
+		
+		return result;
 	}
-
+	
 	
 	@Override
 	public synchronized void close() throws Exception {
@@ -113,6 +113,10 @@ class CoThreadHolder<T> implements AutoCloseable{
 				wait();
 			}
 		}
+	}
+	
+	void closeWithoutWait() {
+		thread.interrupt();
 	}
 }
 
@@ -134,11 +138,11 @@ public class CoThread<T> implements AutoCloseable {
 	
 	@Override
 	public void finalize() throws Exception {
-		close();
+		threadHolder.closeWithoutWait();
 	}
 	
 	/**
-	 * Interrupts the thread. This method is automatically called eventually when the CoThread is garbage collected, but can be called manually if desired. Multiple calls are safe.
+	 * Interrupts the thread. The thread will automatically be interrupted eventually when the CoThread is garbage collected, but can interrupted manually by this method if desired. Calling more than once is safe and will do nothing.
 	 */
 	@Override
 	public void close() throws Exception{
--- src/testDrive/Driver.java
+++ src/testDrive/Driver.java
@@ -3,24 +3,24 @@ import asyncronous.*;
 
 public class Driver {
 	public static void main(String [] args) throws InterruptedException, Exception{
-		CoThread<Integer> co = new CoThread<Integer>(yield -> {
-			yield.accept(0);
-			yield.accept(1);
-			yield.accept(2);
-			yield.accept(3);
-			yield.accept(4);
-			yield.accept(5);
-			yield.accept(6);
-			yield.accept(7);
-		});
-		co.start();
-		
-		
-		while(co.notComplete()) {
-			System.out.println(co.await());
-			Thread.sleep(1);
+		while(true) {
+			CoThread<Integer> co = new CoThread<Integer>(yield -> {
+				yield.accept(0);
+				yield.accept(1);
+				yield.accept(2);
+				yield.accept(3);
+				yield.accept(4);
+				yield.accept(5);
+				yield.accept(6);
+				yield.accept(7);
+			});
+			co.start();
+			
+			
+			for(int i = 0; i < 3; ++i) {
+//				System.out.println(co.await());
+				co.await();
+			}
 		}
-		
-		co.close();
 	}
 }
