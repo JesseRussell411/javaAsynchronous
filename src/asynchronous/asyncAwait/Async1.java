c8c2805f577601377d72f3edfb3134e102c79c8f
jesse russell <jesserussell4@montana.edu>
2021-08-05 13:23:35 -0600

    added functional interfaces that should have been in java to begin with and added for parameterized Async classes

--- src/asynchronous/asyncAwait/Async1.java
+++ src/asynchronous/asyncAwait/Async1.java
@@ -2,23 +2,23 @@ package asynchronous.asyncAwait;
 import java.util.function.*;
 
 import asynchronous.Promise;
-public class Async1<T0, R> {
+public class Async1<T1, R> {
 	private Async<R> async;
 	private Object[] args = new Object[1];
-	public Async1(BiFunction<Async.Await, T0, R> func, String name) {
-		
+	public Async1(BiFunction<Async.Await, T1, R> func, String name) {
 		async = new Async<R>(
-				await -> func.apply(await, (T0)args[0]) , name);
+				await -> func.apply(await, (T1)args[0]) , name);
 	}
 	
-	public Async1(BiFunction<Async.Await, T0, R> func) {
+	public Async1(BiFunction<Async.Await, T1, R> func) {
 		this(func, null);
 	}
 	
-	public synchronized Promise<R> get(T0 t0){
-		args[0] = t0;
+	public synchronized Promise<R> get(T1 t1){
+		args[0] = t1;
 		return async.get();
 	}
+	
 	public String getName() {
 		return async.getName();
 	}
---
+++ src/asynchronous/asyncAwait/Async2.java
@@ -0,0 +1,26 @@
+package asynchronous.asyncAwait;
+import functionPlus.*;
+
+import asynchronous.Promise;
+public class Async2<T1, T2, R> {
+	private Async<R> async;
+	private Object[] args = new Object[2];
+	public Async2(TriFunction <Async.Await, T1, T2, R> func, String name) {
+		async = new Async<R>(
+				await -> func.apply(await, (T1)args[0], (T2)args[1]) , name);
+	}
+	
+	public Async2(TriFunction<Async.Await, T1, T2, R> func) {
+		this(func, null);
+	}
+	
+	public synchronized Promise<R> get(T1 t1, T2 t2){
+		args[0] = t1;
+		args[1] = t2;
+		return async.get();
+	}
+	
+	public String getName() {
+		return async.getName();
+	}
+}
